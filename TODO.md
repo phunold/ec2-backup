b40c32a6570d797ab0481c0ea9f9639a99dfc2ab
Philipp Hunold <philipp.hunold@gmail.com>
2015-04-11 14:35:29 -0400

    run and terminate ubuntu 12.04 instance

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 * P DONE: command line options ec2-backup [-h] [-m method] [-v volume-id] dir
 * G DONE: calculate size of local directory (size X)
 * G: Create new EBS devices of size X
-* P: Create/start/terminate EC2 instance (including EC2_BACKUP_FLAGS_AWS)
+* P DONE: Create/start/terminate EC2 instance (including EC2_BACKUP_FLAGS_AWS)
 * G: Attach EBS to instance
 * G: Method 1: DD utilizing tar(1) on the local host and dd(1) on the remote instance
 * P: Method 2: Rsync to EBS (including EC2_BACKUP_FLAGS_SSH)
--- ec2-backup.py
+++ ec2-backup.py
@@ -7,6 +7,45 @@ import sys
 # FIXME catch ImportError: No module named boto.ec2
 import boto.ec2
 import optparse
+import time
+
+# global settings
+region = 'us-east-1'
+# Ubuntu 12.04 LTS AMD64 EBS
+# http://cloud-images.ubuntu.com/locator/ec2/ 
+ami = 'ami-00615068'
+ssh_user = 'ubuntu'
+
+def connect_ec2(region):
+  # Connect to EC2
+  # print boto.config.__dict__
+  # check if credentials setup
+  # boto.exception.NoAuthHandlerFound: No handler was ready to authenticate. 1 handlers were checked. ['QuerySignatureV2AuthHandler'] Check your credentials
+  try:
+    connection = boto.ec2.connect_to_region(region)
+  except boto.exception.NoAuthHandlerFound:
+    print "Could not authenticate to ec2!"
+    print "run 'aws configure' or setup EC2 keys"
+    sys.exit(1)
+
+  return connection
+
+def create_ec2_instance(connection,ami):
+  reservations = connection.run_instances(ami,key_name='pstam-keypair')
+  # print instance.__dict__
+  # ['RunInstancesResponse', 'region', 'instances', 'connection', 'requestId', 'groups', 'id', 'owner_id']
+  instance = reservations.instances[0] 
+  status = instance.update()
+  while status == 'pending':
+    print 'Waiting for instance to be ready:', status
+    time.sleep(5)
+    status = instance.update()
+  
+  # add EC2 Tagname
+  instance.add_tag('Name', 'EC2-Backup Helper Instance')
+
+  # return the freshly create instance object
+  return instance
 
 # calculate the volume size
 def estimate_size(path):
@@ -22,8 +61,6 @@ def estimate_size(path):
   # [:-1] returns everything but the last character 
   return proc.stdout.readline().split()[0][:-1]
 
-  #print "size in GB (rounded up):", estimate_size(sys.argv[1])
-  #sys.exit(1)
 def Main():
 
   # 
@@ -73,6 +110,27 @@ def Main():
   # 
   print 'Estimate Directory size in GB: %s' % estimate_size(backupdir)
 
+  #
+  # run new EC2 instance 
+  # 
+  connection = connect_ec2(region)
+
+  server_instance = create_ec2_instance(connection, ami)
+
+  fqdn = server_instance.public_dns_name
+  print 'server hostname: ', fqdn
+
+  #
+  # SSH/TAR/Stuff
+  #
+
+  #
+  # finally terminate the instance
+  #
+  print 'Cleanup time ID terminating: ', server_instance.id
+  time.sleep(10)
+  connection.terminate_instances(instance_ids=[server_instance.id])
+ 
   sys.exit(0)
 
 def print_env():
@@ -88,30 +146,6 @@ def print_env():
   if 'EC2_PRIVATE_KEY' in os.environ:
     print os.environ['EC2_PRIVATE_KEY']
 
-
-def myec2():
-  # Connect to EC2
-  # print boto.config.__dict__
-  # check if credentials setup
-  # boto.exception.NoAuthHandlerFound: No handler was ready to authenticate. 1 handlers were checked. ['QuerySignatureV2AuthHandler'] Check your credentials
-  try:
-    ec2 = boto.ec2.connect_to_region("us-east-1")
-  except boto.exception.NoAuthHandlerFound:
-    print "Could not authenticate to ec2!"
-    print "run 'aws configure' or setup EC2 keys"
-    sys.exit(1)
-
-  # FIXME should use 'ec2.get_all_reservations' but this boto version seems does not have it
-  reservations = ec2.get_all_instances()
-  for reservation in reservations:
-    for instance in reservation.instances:
-      instance_name = instance.tags['Name']
-      instance_id = instance.id
-      ip = instance.ip_address
-      state = instance.state
-      ami = instance.image_id
-  #   print instance.__dict__.keys()
-      print "Name:", instance_name, "ID:", instance_id, "IP:", ip, "State:", state, "AMI:", ami
 
 
 
